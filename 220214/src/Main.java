a420b97d13972c2410bfd451d8a69775421d828d
jaejun <jkds878@naver.com>
2022-02-14 22:44:40 +0900

    baekjoon0214

---
+++ 220214/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		double num1 = sc.nextInt();
+		double num2 = sc.nextInt();
+		
+		System.out.println(num1/num2);
+	}
+
+}
