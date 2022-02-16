700d39ba305ad13d1bc47d49821a95ecf0c1953d
jaejun <jkds878@naver.com>
2022-02-16 20:40:17 +0900

    220216baekjoon

--- 220214/src/Main.java
+++ 220214/src/Main.java
@@ -4,12 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		
-		double num1 = sc.nextInt();
-		double num2 = sc.nextInt();
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
 		
-		System.out.println(num1/num2);
+		int a = num2%10;
+		int b = (num2/10)%10;
+		int c = ((num2/10)/10)%10;
+		
+		System.out.println(num1*a);
+		System.out.println(num1*b);
+		System.out.println(num1*c);
+		System.out.println(num1*num2);
 	}
 
 }
