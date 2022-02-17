8a5c8c509a918a153b12350aee10867e5b6fee3f
jaejun <jkds878@naver.com>
2022-02-17 17:46:29 +0900

    220217 baekjoon

--- 220214/src/Main.java
+++ 220214/src/Main.java
@@ -8,16 +8,23 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
+				
+		if(num1 > 89 && num1 < 101 ) {
+			System.out.println("A");
+		}
+		else if(num1 > 79 && num1 < 90) {
+			System.out.print("B");
+		}
+		else if(num1 > 69 && num1 < 80) {
+			System.out.println("C");
+		}
+		else if(num1 > 59 && num1 < 70) {
+			System.out.println("D");
+		}
+		else {
+			System.out.println("F");
+		}
 		
-		int a = num2%10;
-		int b = (num2/10)%10;
-		int c = ((num2/10)/10)%10;
-		
-		System.out.println(num1*a);
-		System.out.println(num1*b);
-		System.out.println(num1*c);
-		System.out.println(num1*num2);
 	}
 
 }
